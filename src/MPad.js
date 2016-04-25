653ef7a1c6de59ffc7875df0d1137efb626456dc
mmqq8707 <276104867@qq.com>
2016-04-25 16:59:19 +0800

    redux

---
+++ src/MPad.js
@@ -0,0 +1,40 @@
+/**
+ * Created by liu on 2016/4/25.
+ */
+import React, {
+    AppRegistry,
+    Platform,
+    } from 'react-native';
+import thunkMiddleware from 'redux-thunk';
+import { Provider } from 'react-redux'
+import { createStore, applyMiddleware, combineReducers } from 'redux';
+import rootReducer from './reducers/index';
+const createStoreWithMiddleware = applyMiddleware(thunkMiddleware)(createStore);
+const store = createStoreWithMiddleware(rootReducer);
+
+import MainScreenContainer from './containers/MainScreenContainer';
+export default function native(platform) {
+
+    const MPad = React.createClass ({
+        render() {
+            if (Platform.OS === 'android') {
+                console.log(platform)
+                return (
+                    <Provider store={store}>
+
+                        <MainScreenContainer/>
+
+                    </Provider>
+
+                );
+
+            }
+            else {
+
+            }
+        }
+    })
+
+    AppRegistry.registerComponent('MPad', () => MPad);
+
+}
