ab4c772b9e856debf2b6e927a3d52f71a924c3cb
mmqq8707 <276104867@qq.com>
2016-04-27 17:47:23 +0800

    hapi

---
+++ service/server.js
@@ -0,0 +1,42 @@
+'use strict';
+
+const Hapi = require('hapi');
+
+const server = new Hapi.Server();
+server.connection({ port: 3000 });
+
+server.route({
+    method: 'GET',
+    path: '/',
+    handler: function (request, reply) {
+        reply('Hello, world!');
+    }
+});
+
+server.route({
+    method: 'GET',
+    path: '/{name}',
+    handler: function (request, reply) {
+        reply('Hello, ' + encodeURIComponent(request.params.name) + '!');
+    }
+});
+server.route({
+    method: 'GET',
+    path: '/hello/{user?}',
+    handler: function (request, reply) {
+        const user = request.params.user ? encodeURIComponent(request.params.user) : 'stranger';
+        reply('Hello ' + user + '!');
+    },
+    config: {
+        description: 'Say hello!',
+        notes: 'The user parameter defaults to \'stranger\' if unspecified',
+        tags: ['api', 'greeting']
+    }
+});
+server.start((err) => {
+
+    if (err) {
+        throw err;
+    }
+    console.log('Server running at:', server.info.uri);
+});
